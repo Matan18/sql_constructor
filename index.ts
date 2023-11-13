36410b34ed416147ff385062f2d90a871a8cd99c
Mateus Andriola <mateusandriola18@gmail.com>
2023-11-13 08:42:53 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,196 @@
+import { BindParameter, DBObject_IN } from "oracledb";
+
+class FailBindData extends Error {}
+
+type BindParam =
+  | string
+  | number
+  | BindParameter
+  | Date
+  | DBObject_IN<any>
+  | Buffer
+  | null
+  | undefined
+  | { [key: string]: BindParam };
+type ConstructorReturn = { query: string; bind: Record<string, BindParam> };
+
+function generateRandomString(length = 6) {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; // characters to include in the random string
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+function constructBind() {
+  // const bindCache = new Map<BindParam, string>();
+  const bind: Record<string, BindParam> = {};
+  // let count = 0;
+  // function getBindCount() {
+  //   const key = `bind${count}`;
+  //   count++;
+
+  //   return key;
+  // }
+
+  function addBind(value: BindParam, specificKey?: string) {
+    // const key = bindCache.get(value) || getBindCount();
+    const key = specificKey || generateRandomString();
+
+    bind[key] = value;
+
+    // bindCache.set(value, key);
+
+    return `:${key}`;
+  }
+
+  function concatBind(binds: Record<string, BindParam>) {
+    Object.assign(bind, binds);
+  }
+
+  return { bind, addBind, concatBind };
+}
+
+type Param = BindParam | BindParam[] | ConstructorReturn | ConstructorReturn[];
+
+export function sql(queries: TemplateStringsArray, ...params: Param[]) {
+  const { bind, addBind, concatBind } = constructBind();
+
+  if (params.length === 0) return { query: queries.join(""), bind };
+
+  function handleQueryParam(query: string, param?: Param) {
+    if (!param && param !== 0 && param !== null) return query;
+
+    if (Array.isArray(param)) {
+      if (param.length === 0) return query;
+
+      const first = param[0];
+      if (
+        typeof first === "bigint" ||
+        typeof first === "number" ||
+        typeof first === "string"
+      ) {
+        return `${query}${(param as unknown as string[])
+          .map((item) => addBind(item))
+          .join(",")}`;
+      }
+
+      if (typeof first === "object") {
+        if (
+          first &&
+          Object.keys(first).includes("query") &&
+          Object.keys(first).includes("bind")
+        ) {
+          const innerQuery = first as unknown as {
+            query: string;
+            bind: Record<string, BindParam>;
+          };
+          concatBind(innerQuery.bind);
+
+          return `${query}${innerQuery.query}`;
+        }
+      }
+
+      return query;
+    }
+
+    if (
+      typeof param === "string" ||
+      typeof param === "number" ||
+      typeof param === "bigint" ||
+      param === null
+    ) {
+      return `${query}${addBind(param)}`;
+    }
+
+    if (typeof param === "object") {
+      if (
+        param &&
+        Object.keys(param).includes("query") &&
+        Object.keys(param).includes("bind")
+      ) {
+        const innerQuery = param as unknown as {
+          query: string;
+          bind: Record<string, BindParam>;
+        };
+        concatBind(innerQuery.bind);
+
+        return `${query}${innerQuery.query}`;
+      }
+
+      if (Object.keys(param).length === 1) {
+        const [key, value] = Object.entries(param)[0];
+
+        return `${query}${addBind(value, key)}`;
+      }
+    }
+
+    if (
+      typeof param === "function" ||
+      typeof param === "symbol" ||
+      typeof param === "object"
+    ) {
+      throw new FailBindData(
+        "Parâmetro é um objeto, deveria ser um array ou um valor puro"
+      );
+    }
+
+    return query;
+  }
+
+  const query = queries
+    .map((item, index) => {
+      if (index === 0 && !Array.isArray(params)) {
+        return `${item}${addBind(params)}`;
+      }
+      const param = params[index];
+
+      return handleQueryParam(item, param);
+    })
+    .join("");
+
+  return { query, bind };
+}
+
+export function joinSql(constructors: ConstructorReturn[], joinString: string) {
+  return constructors.reduce(
+    (acc, { bind, query }, index) => {
+      if (index === 0) return { bind, query };
+
+      return {
+        bind: { ...acc.bind, ...bind },
+        query: `${acc.query}${joinString}${query}`,
+      };
+    },
+    { query: "", bind: {} } as ConstructorReturn
+  );
+}
+
+function concatBind(
+  bind1: Record<string, BindParam>,
+  { query, bind }: ConstructorReturn
+): ConstructorReturn {
+  const bindKeys = Object.keys(bind1);
+  return Object.keys(bind).reduce(
+    (acc, bindItem, index) => {
+      const key = `bind${index + bindKeys.length}`;
+      const parsedQuery = acc.query.replace(new RegExp(bindItem, "g"), key);
+      console.log({ bindItem, key });
+      console.log("query", acc.query);
+      console.log("parsed", parsedQuery);
+
+      return {
+        query: parsedQuery,
+        bind: {
+          ...acc.bind,
+          [key]: bind[bindItem],
+        },
+      };
+    },
+    { bind: bind1, query }
+  );
+}
